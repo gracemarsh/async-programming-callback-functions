770f421d2da1ccc367e5a69fa18d48f7e045ad84
Grace <gracemarsh27@gmail.com>
2021-08-13 20:48:26 +1000

    Update callbacks.js

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,14 +1,14 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
+const astrosUrl = "http://api.open-notify.org/astros.json";
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+const peopleList = document.getElementById("people");
+const btn = document.querySelector("button");
 
 // Make an AJAX request
 function getJSON(url) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
+  xhr.open("GET", url);
   xhr.onload = () => {
-    if(xhr.status === 200) {
+    if (xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
       console.log(data);
     }
@@ -18,10 +18,10 @@ function getJSON(url) {
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
+  const section = document.createElement("section");
   peopleList.appendChild(section);
   // Check if request returns a 'standard' page from Wiki
-  if (data.type === 'standard') {
+  if (data.type === "standard") {
     section.innerHTML = `
       <img src=${data.thumbnail.source}>
       <h2>${data.title}</h2>
@@ -37,3 +37,5 @@ function generateHTML(data) {
     `;
   }
 }
+
+getJSON(astrosUrl);
